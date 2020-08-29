6bfcbaf2ec7847a31a507428daa0dcfbfdf5c45a
gurjotsaini <gurjotsaini1992@gmail.com>
2020-08-29 15:21:58 +0530

    Added: UserResource

--- app/Http/Controllers/User/MeController.php
+++ app/Http/Controllers/User/MeController.php
@@ -3,24 +3,23 @@
 namespace App\Http\Controllers\User;
 
 use App\Http\Controllers\Controller;
+use App\Http\Resources\UserResource;
 use Illuminate\Http\JsonResponse;
-use Illuminate\Http\Request;
 
 class MeController extends Controller
 {
     /**
      * Return current authenticated user
      *
-     * @return JsonResponse
+     * @return UserResource|JsonResponse
      */
     public function getMe() {
         if (auth()->check()) {
             $user = auth()->user();
-            return response()->json(["user" => $user], 200);
+            return new UserResource($user);
+//            return response()->json(["user" => $user], 200);
         }
 
         return response()->json(null, 200);
     }
 }
-
-//            return response()->json(['user' => auth()->user()], 200);
